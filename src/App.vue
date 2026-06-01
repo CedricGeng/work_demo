da8506a4ebc5780532805c210aa46d60ec50983e
CedricGeng <zc_works@163.com>
2026-06-02 02:31:22 +0800

    新增公共组件,导入新组件

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const message = ref('Hello, Vue 3 with TypeScript!')
-function onceshi() {
-  message.value = 'Button clicked!'
-}
+  import Person from "./components/Person.vue"
 </script>
 
 <template>
-  <div class="web">You did it!</div>
-  <div class="web">You did it!</div>
-  <div class="web">{{ message }}</div>
-  <button @click="onceshi">{{ message }}</button>
+  <Person />
 </template>
 
 <style scoped>
-.web {
-  background-color: #f0f0f0;
-  padding: 20px;
-  border-radius: 8px;
-  color: red;
-  text-align: center;
-  font-size: 24px;
-}
+
 </style>
---
+++ src/components/Person.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+ import { ref } from 'vue'
+ let message = ref('Hello, Vue 3 with TypeScript!')
+</script>
+
+<template>
+  <h1 class="person-title">{{message}}</h1>
+</template>
+
+<style scoped>
+.person-title {
+  color: blue;
+  font-size: 24px;
+}
+</style>
