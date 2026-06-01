76bfa2cda9bdeca5122102551fa0e91e01894c2e
CedricGeng <zc_works@163.com>
2026-06-02 02:18:10 +0800

    测试ref与button事件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-  
+import { ref } from 'vue'
+const message = ref('Hello, Vue 3 with TypeScript!')
+function onceshi() {
+  message.value = 'Button clicked!'
+}
 </script>
 
 <template>
-  <!-- <h1 class="web">You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p> -->
   <div class="web">You did it!</div>
   <div class="web">You did it!</div>
+  <div class="web">{{ message }}</div>
+  <button @click="onceshi">{{ message }}</button>
 </template>
 
 <style scoped>
