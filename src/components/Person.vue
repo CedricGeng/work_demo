73b542bcb5ecaef9b51374faa69235bf42b299a7
CedricGeng <zc_works@163.com>
2026-06-02 17:48:11 +0800

    优化Person组件中的v-if结构，调整代码格式

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { ref, reactive, onMounted } from "vue"
-
+let show = ref("a")
 </script>
 
 <template>
-
+  <div v-if="show == 'c'">测试Vue的v-if1</div>
+  <div v-else-if="show == 'a'">测试Vue的v-if2</div>
+  <div v-else="show == 'v'">测试Vue的v-if3</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
