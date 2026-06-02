dd2b24bc8142dd316753405f8ba841ba8bf813af
CedricGeng <zc_works@163.com>
2026-06-02 14:26:08 +0800

    reactive优势

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,19 +1,27 @@
-<script setup lang="ts" name="personComponent">
-import { ref } from "vue"
-let username = ref("John Doe")
-let age = ref(30)
+<script setup lang="ts">
+import { ref, reactive, onMounted } from "vue"
+let person = reactive({
+  name: "John Doe",
+  age: 30,
+  email: "john.doe@example.com",
+  residence: {
+    city: "New York",
+    country: "USA"
+  }
+})
+onMounted(() => {
+  console.log("person:", person)
+  console.log("Person component mounted")
+})
 </script>
-
 <template>
-  <h1 class="person-title">{{ username }}</h1>
-  <input v-model="age" placeholder="Enter name" />
-  <p class="person-age">{{ age }} years old</p>
-  <button @click="age++">Increase Age</button>
+  <h2>Person Information</h2>
+  <p>Name: {{ person.name }}</p>
+  <p>Age: {{ person.age }}</p>
+  <p>Email: {{ person.email }}</p>
+  <p>City: {{ person.residence.city }}</p>
+  <p>Country: {{ person.residence.country }}</p>
+  <input v-model="person.residence.city" placeholder="Enter city" />
 </template>
 
-<style scoped>
-.person-title {
-  color: blue;
-  font-size: 24px;
-}
-</style>
+<style scoped></style>
