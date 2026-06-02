b58c0af6bd44aed5e32c31eb6f10f500b5350202
CedricGeng <zc_works@163.com>
2026-06-02 15:26:24 +0800

    v-for属性的使用

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
 import { ref, reactive, onMounted } from "vue"
-let onClicks = (e: Event) => {
-  console.log(e)
-}
+let List = ref([
+  {
+    name: "张三",
+    age: 18,
+  },
+  {
+    name: "李四",
+    age: 20,
+  },
+  {
+    name: "王五",
+    age: 22,
+  },
+])
 </script>
 <template>
-  <button @click="onClicks">Click me</button>
+  <div>
+    <h1>人员列表</h1>
+    <ul>
+      <li v-for="(item, index) in List" :key="index">
+        {{ item.name }} - {{ item.age }}岁
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
