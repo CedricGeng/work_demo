83d50d8ab2c0e5ecb54643f551f9066d3d40b55f
CedricGeng <zc_works@163.com>
2026-06-02 17:35:42 +0800

    移除Person组件中的示例数据和表格结构

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,22 +1,12 @@
 <script setup lang="ts">
 import { ref, reactive, onMounted } from "vue"
 
-let list = reactive([
-  {
-    name: "张三",
-  },
-])
 </script>
 
 <template>
-  <el-table :data="list" style="width: 100%">
-    <el-table-column
-      v-for="item in list"
-      :key="item.name"
-      label="姓名"
-      width="180"
-    />
-  </el-table>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
