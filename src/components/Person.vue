3308115339e7463bde37aa124902252e832e8312
CedricGeng <zc_works@163.com>
2026-06-04 03:41:24 +0800

    暂时结束

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,8 +1,24 @@
 <template>
+  <!-- 子组件 -->
+  子组件：{{ doubleCount }}
+  <el-button type="primary" @click="sendMsg">点击向父组件传递数据</el-button>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive, onMounted, computed, watch } from "vue"
+let props = defineProps({
+  count: {
+    type: Number,
+    required: true,
+  },
+})
+const doubleCount = computed(() => props.count * 2)
+
+const emit = defineEmits(["getChildData"])
+const sendMsg = () => {
+  // 2. emit(事件名, 参数)
+  emit(" ", doubleCount.value)
+}
 </script>
 
 <style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,23 @@
 <template>
+  <!-- 父组件 -->
   <div class="home">
-    <h1>{{ storeId.count }}</h1>
-    <el-button type="primary" @click="storeId.count++">+</el-button>
-    <el-button type="primary" @click="storeId.count--">-</el-button>
+    <h1>{{ count }}</h1>
+    <el-button type="primary" @click="count++">+</el-button>
+    <el-button type="primary" @click="count--">-</el-button>
+    <Person :count="count" @getChildData="handleChildData" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive, onMounted, computed, watch } from "vue"
+import Person from "@/components/Person.vue"
 import useStore from "@/store/index"
-const storeId = useStore()
+import { storeToRefs } from "pinia"
+const { count } = storeToRefs(useStore())
+const handleChildData = (data: { name: string; age: number }) => {
+  // console.log("父组件接收到的数据:", data)
+  alert(`父组件接收到的数据: ${JSON.stringify(data)}`)
+}
 </script>
 
 <style scoped></style>
