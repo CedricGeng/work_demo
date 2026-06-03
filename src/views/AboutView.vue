bc9d28bc737891317efe53ec1f4024a0aafdfabd
CedricGeng <zc_works@163.com>
2026-06-03 19:26:59 +0800

    重置

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,31 +1,11 @@
 <template>
   <div class="about">
-    <el-container>
-      <el-header>
-        <RouterLink :to="`/about/a?list=${JSON.stringify(list)}`"
-          >Go to About A</RouterLink
-        >
-        <RouterLink to="/about/b">Go to About B</RouterLink>
-      </el-header>
-      <el-main>
-        <RouterView />
-      </el-main>
-    </el-container>
+    <h1>This is an about page</h1>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive, onMounted, computed, watch } from "vue"
-const list = reactive([
-  {
-    name: "About A",
-    age: 18,
-  },
-  {
-    name: "About B",
-    age: 20,
-  },
-])
 </script>
 
 <style scoped></style>
--- src/views/AboutViewB.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="home">
-    <h1>This is a AboutB page</h1>
-  </div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
